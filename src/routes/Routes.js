d0c6a7738385c6a927dd2ac209895cf0c21c077c
Mikael Larsson <73987306+84miklar@users.noreply.github.com>
2021-11-23 09:39:10 +0100

    La till ett rum och gåta

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { HomeView } from '../views/HomeView';
+import { RoomOneView } from '../views/roomOne/RoomOneView';
 export const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path='/' component={HomeView} />
+        <Route exact path='/roomone' component={RoomOneView} />
       </Switch>
     </BrowserRouter>
   );
